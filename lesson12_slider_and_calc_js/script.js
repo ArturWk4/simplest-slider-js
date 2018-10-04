e7ce4c9ad5b7baefe7f83ab9855af359a21fd1c1
Artur <elkin.as@mail.ru>
2018-10-04 22:55:39 +0300

    base implementation without pretty styles

---
+++ lesson12_slider_and_calc_js/script.js
@@ -0,0 +1,51 @@
+let slideIndex = 1,
+    slides = document.getElementsByClassName('slider-item'),
+    prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.getElementsByClassName('dot');
+
+function showSlides(index) {
+    if (index > slides.length) {
+        slideIndex = 1;
+    }
+    if (index < 1) {
+        slideIndex = slides.length;
+    }
+    for (let slide of slides) {
+        slide.style.display = 'none';
+    }
+    for (let dot of dots) {
+        dot.classList.remove('dot-active');
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+
+}
+
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+prev.addEventListener('click', function () {
+    moveSlide(-1);
+});
+
+next.addEventListener('click', function () {
+    moveSlide(1);
+});
+
+dotsWrap.addEventListener('click', function (event) {
+    for (let i = 0; i < dots.length + 1; ++i) {
+        if (event.target.classList.contains('dot') && event.target === dots[i - 1]) {
+            currentSlide(i);
+        }
+    }
+});
